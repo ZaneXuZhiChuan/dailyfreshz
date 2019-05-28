0ffc769cfb2429457c5194e4bdf3f1301fc02631
xuzhichuan <x18907084487@gmail.com>
2019-05-28 08:34:15 +0800

    完成index界面src的替换，新增脚本subTxt

---
+++ dailyfresh/utils/subTxt.py
@@ -0,0 +1,36 @@
+from django.conf import settings
+import django
+
+# 预编译 django 文件
+import os
+import re
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dailyfresh.settings")
+django.setup()
+
+'''将Index中固定的src地址更改为static形式'''
+
+
+# <a href="#"><img src="images/goods/goods019.jpg"></a>
+# {% static 'images/goods/goods012.jpg' %}
+# {% static '%s' %}
+class changeIndexSrc:
+    index_html = os.path.join(settings.BASE_DIR, 'templates/index.html')
+    save_path = os.path.join(settings.BASE_DIR, 'templates/index.html')
+    pattern = '<img src="images/([\w/])*\.jpg">'
+    with open(index_html, 'r') as f:
+        with open(save_path, 'w') as f2:
+            for line in f:
+                # pat = re.compile(r'<img src="(images/[\w/]*\.jpg)">')
+                pat = re.compile(r'src="([\w/\.]*)">')
+                res = pat.findall(line)
+                if len(res) > 0:
+                    for r in res:
+                        rp = "{% static '" + r + "' %}"
+                        f2.write(re.sub(r, rp, line))
+                else:
+                    f2.write(line)
+
+
+if __name__ == '__main__':
+    changeIndex = changeIndexSrc()
